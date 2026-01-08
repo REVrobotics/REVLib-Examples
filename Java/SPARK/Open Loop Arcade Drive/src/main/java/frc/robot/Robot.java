e26376fe8e51cc319c71e180b8d145d2b7b0a8ac
Dave Oleksy <dave.oleksy@revrobotics.com>
2026-01-08 12:53:40 -0600

    Update Open Loop Arcade example for 2026

--- Java/SPARK/Open Loop Arcade Drive/src/main/java/frc/robot/Robot.java
+++ Java/SPARK/Open Loop Arcade Drive/src/main/java/frc/robot/Robot.java
@@ -8,8 +8,8 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
